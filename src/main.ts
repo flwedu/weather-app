0b554107d64d96ccb66598ac0ea41110e2ca1ba1
Eduardo Aquino <55285816+flwedu@users.noreply.github.com>
2023-04-29 23:43:17 -0300

    ux: add toggle dark mode function

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,7 @@
 import './style.css'
 
+const buttonDarkMode = document.getElementById("toggle-dark-mode")!;
+buttonDarkMode.addEventListener("click", () => {
+    buttonDarkMode.classList.toggle("active");
+    document.body.classList.toggle("dark");
+});
