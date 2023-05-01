f1438b11074ce80e2db11e41c97cf1d1e4d803e5
Eduardo Aquino <55285816+flwedu@users.noreply.github.com>
2023-05-01 10:58:59 -0300

    feat: uses gps location to get coordinates - add button-add-location - adjust some ui styles

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,33 @@ import './style.css'
 import {CityCard} from "./model/city-card.ts";
 import {DataRequest} from "./model/data-request.ts";
 
-const buttonDarkMode = document.getElementById("toggle-dark-mode")!;
-const searchForm = document.forms.namedItem("search__card")!;
-buttonDarkMode.addEventListener("click", () => {
-    buttonDarkMode.classList.toggle("active");
+const searchForm = document.forms.namedItem("search-form")!;
+const searchInput = document.getElementById("search-input") as HTMLInputElement;
+const buttonGps = document.getElementById("button-gps")!;
+const resultsDiv = document.getElementById("results")!;
+document.getElementById("toggle-dark-mode")!.addEventListener("click", (e) => {
+    const button = e.target as HTMLButtonElement;
+    button.classList.toggle("active");
     document.body.classList.toggle("dark");
 });
 
+document.getElementById("button-add-location")!.addEventListener("click", (e) => {
+    const searchCard = document.getElementById("search-card")!;
+    searchCard.classList.toggle("active");
+})
+
+buttonGps.addEventListener("click", () => {
+    navigator.geolocation.getCurrentPosition((loc) => {
+        const {latitude,longitude} = loc.coords;
+        searchInput.value = `${latitude},${longitude}`;
+    });
+})
+
 searchForm.addEventListener("submit", (event) => {
     event.preventDefault();
-    const input = searchForm.querySelector("#search__input") as HTMLInputElement;
-    const cityName = input.value;
+    const cityName = searchInput.value;
     new DataRequest({cityName}).get().then((res) => {
         const cityCard = new CityCard(res);
-        document.body.append(cityCard.render());
+        resultsDiv.prepend(cityCard.render());
     });
 })
