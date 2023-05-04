e0a8b564cebc067f7bdd9a4486914c96fd295f3e
Eduardo Aquino <55285816+flwedu@users.noreply.github.com>
2023-05-03 22:27:53 -0300

    wip: add getForecast() to DataRequest class

--- src/model/data-request.ts
+++ src/model/data-request.ts
@@ -4,9 +4,16 @@ export class DataRequest {
     private BASE_URL = import.meta.env.VITE_WEATHER_BASE_URL;
     private API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
 
-    public async get(query: string){
+    public async get(query: string): Promise<CurrentForecast>{
         const url = `${this.BASE_URL}/current.json?key=${this.API_KEY}&q=${query}&lang=pt`;
         const response = await fetch(url);
         return response.json();
     }
+
+		public async getForecast(query: string): Promise<NextDaysForecast>{
+			const days = 5;
+			const url = `${this.BASE_URL}/forecast.json?key=${this.API_KEY}&q=${query}&days=${days}&lang=pt`;
+			const response = await fetch(url);
+			return response.json();
+		}
 }
