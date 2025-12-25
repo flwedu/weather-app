834f514b46cc489240b3a6605ae344805b7de194
Eduardo Ribeiro <eduardo.ribeiro@gtplan.com.br>
2025-12-25 11:03:14 -0300

    feat: define ViteTypeOptions and ImportMetaEnv interfaces for environment variables

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,12 @@
-/// <reference types="vite/client" />
+interface ViteTypeOptions {
+	strictImportMetaEnv: unknown;
+}
+
+interface ImportMetaEnv {
+	readonly VITE_WEATHER_BASE_URL: string;
+	readonly VITE_WEATHER_API_KEY: string;
+}
+
+interface ImportMeta {
+	readonly env: ImportMetaEnv;
+}
